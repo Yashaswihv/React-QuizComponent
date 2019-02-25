533be18b38f79f0a28c6527daec265bb1966c77a
z001shw <Yashaswi.Hv@Target.com>
2019-02-25 19:20:40 +0530

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component{
 	}
 	render() {
 		return (
-			<div classname = "QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+			<div classname = "QuizQuestion">Quiz</div>
 			)
 	}
 }
